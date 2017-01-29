1c2e7aa589ddf11f20f093c932a0974e30ba3a77
Daniel Steinmetz <daniel.steinmetz@posteo.de>
2017-01-29 12:32:55 +0100

    started internal board tests

--- GameServerVorgabe/src/de/tuberlin/sese/swtpp/gameserver/test/lasca/LascaBoardTests.java
+++ GameServerVorgabe/src/de/tuberlin/sese/swtpp/gameserver/test/lasca/LascaBoardTests.java
@@ -1,9 +1,22 @@
 package de.tuberlin.sese.swtpp.gameserver.test.lasca;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.tuberlin.sese.swtpp.gameserver.control.GameController;
+import de.tuberlin.sese.swtpp.gameserver.model.Player;
+import de.tuberlin.sese.swtpp.gameserver.model.User;
+import de.tuberlin.sese.swtpp.gameserver.model.lasca.LascaGame;
+
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.tuberlin.sese.swtpp.gameserver.model.lasca.LascaBoard;
@@ -11,18 +24,32 @@ import de.tuberlin.sese.swtpp.gameserver.model.lasca.LascaField;
 
 public class LascaBoardTests {
 
+	LascaBoard board = null;
+	
+	@Before
+	public void setUp() throws Exception{
+		board = new LascaBoard("b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w");
+	}
+	
+	
 	@Test
-	public void testSize() {
-		String fen = "b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w";
-		LascaBoard board = new LascaBoard(fen);
-		
-		assertEquals(board.getFields().size(), 25);
-		assertEquals(board.toFenString(), fen);
+	public void testValidStartingBoard_shouldPass(){
+		String startingBoard = "b,b,b,b/b,b,b/b,b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w";
+		String fenCurrentBoard = board.toFenString();
+		assertEquals(startingBoard, fenCurrentBoard);	
 	}
 	
 	@Test
+	public void testValidStartingBoard_shouldFail(){
+		String startingBoard = "b,b,b,b/b,b,b/b,b,b/,,/w,w,w,w/w,w,w/w,w,w,w";
+		String fenCurrentBoard = board.toFenString();
+		assertNotEquals(startingBoard, fenCurrentBoard);
+	}
+	
+	
+	
+	@Test
 	public void testInvalidBoard_TooManyFigures(){
-		String fen = "b,b,b,b/b,b,b/b,b,b,b/b,b,/w,w,w,w/w,w,w/w,w,w,w";
 		// how should invalid state be tested with the given function?
 	}
 
